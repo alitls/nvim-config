167c205c43d6848fe50d7bd2cbfaf7b436086393
alitls <alitls8800@gmail.com>
2025-05-28 19:03:44 +0330

    add init.lua

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- Basic Settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.termguicolors = true
+
+-- Key mappings
+vim.g.mapleader = " "
